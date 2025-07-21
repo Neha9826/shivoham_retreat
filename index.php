aa54a45884418a60398247c207f2f84d23c71186
Neha <86908898+Neha9826@users.noreply.github.com>
2025-07-21 18:32:55 +0530

    Project created converted into php and view page setting, user validation, meal plan CRUD done

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+<?php include 'includes/head.php'; ?>
+
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <?php include 'includes/header.php'; ?>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <?php include 'includes/slider.php'; ?>
+    <!-- slider_area_end -->
+
+    <!-- about_area_start -->
+    <?php include 'includes/about_1.php'; ?>
+    <!-- about_area_end -->
+
+    <!-- offers_area_start -->
+    <?php include 'includes/offers.php'; ?>
+    <!-- offers_area_end -->
+
+    <!-- video_area_start -->
+    <?php include 'includes/video_area.php'; ?>
+    <!-- video_area_end -->
+
+    <!-- about_area_start -->
+    <?php include 'includes/about_2.php'; ?>
+    <!-- about_area_end -->
+
+    <!-- features_room_startt -->
+    <?php include 'includes/features_room.php'; ?>
+    <!-- features_room_end -->
+
+    <!-- forQuery_start -->
+    <?php include 'includes/forQuery.php'; ?>
+    <!-- forQuery_end-->
+
+    <!-- instragram_area_start -->
+    <?php include 'includes/insta_area.php'; ?>
+    <!-- instragram_area_end -->
+
+    <!-- footer -->
+    <?php include 'includes/footer.php'; ?>
+
+    <!-- link that opens popup -->
+
+    <!-- form itself end-->
+    <?php include 'includes/form.php'; ?>
+    <!-- form itself end -->
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/gijgo.min.js"></script>
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+
+    <script src="js/main.js"></script>
+    <script>
+        $('#datepicker').datepicker({
+            iconsLibrary: 'fontawesome',
+            icons: {
+             rightIcon: '<span class="fa fa-caret-down"></span>'
+         }
+        });
+        $('#datepicker2').datepicker({
+            iconsLibrary: 'fontawesome',
+            icons: {
+             rightIcon: '<span class="fa fa-caret-down"></span>'
+         }
+
+        });
+    </script>
+
+
+
+</body>
+
+</html>
